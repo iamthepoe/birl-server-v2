1b731fba0ba57ffcd3315369958dcb7e494cbd5d
iamthepoe <luca1901p@gmail.com>
2023-05-28 15:56:17 -0300

    changing export

--- src/interpreter/birlToC.ts
+++ src/interpreter/birlToC.ts
@@ -4,7 +4,7 @@
  * para C.
  *
  ***********************************************************************/
-export default function (birlCode) {
+function birlToC(birlCode) {
   // A tradução é feita com um simples replace no código birl com o seu respectivo valor
   //em C, a regex (?=(?:[^"]|"[^"]*")*$) evita que sejam substituido os valores dentro
   //de aspas.
@@ -98,3 +98,5 @@ export default function (birlCode) {
 
   return code;
 };
+
+export {birlToC};
