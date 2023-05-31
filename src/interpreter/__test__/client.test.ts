733dd7aee11d6de158c704d336d9a0b9e81112f8
iamthepoe <lucagamerbrime@gmail.com>
2023-05-31 10:11:07 -0300

    HORA DO SHOW test

--- src/interpreter/__test__/client.test.ts
+++ src/interpreter/__test__/client.test.ts
@@ -4,8 +4,15 @@ import { BirlClient } from "../BirlClient";
 const birlClient = new BirlClient();
 
 describe('convertToC method', ()=>{
+    const imports = '#include <stdio.h>\n#include <math.h>\n\n';
+    
     it('should return a string value', ()=>{
         const code = birlClient['convertToC']('HORA DO SHOW\nBIRL');
         assert.equal(typeof code, 'string');
+    });
+
+    it(`should return "int main(void){" `, ()=>{
+        const code = birlClient['convertToC']('HORA DO SHOW');
+        assert.equal(imports + 'int main (void) {', code);
     });
 })
